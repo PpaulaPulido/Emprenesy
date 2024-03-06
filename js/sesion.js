e174776f2c0bcb12b94e0e27267f875dccda3aa0
pulidoibarra15@gmail.com <pulidoibarra15@gmail.com>
2024-03-06 10:13:38 -0500

    creacion de popup

---
+++ js/sesion.js
@@ -0,0 +1,16 @@
+function iniciar_sesion(){
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Has iniciado sesión correctamente",
+        showConfirmButton: false,
+        timer: 2000,
+        customClass: {
+            popup: 'border-blue',// Clase CSS para el borde del SweetAlert
+            icon: 'success-icon',
+          }
+        
+      }).then(function(){
+        window.location.href = '../html/index.html';
+      })
+}

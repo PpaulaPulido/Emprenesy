0851911bb92ea223a36752aa146312da4c619b46
pulidoibarra15@gmail.com <pulidoibarra15@gmail.com>
2024-03-27 19:34:34 -0500

    renombrar carpeta de html

--- js/datos.js
+++ js/datos.js
@@ -3,7 +3,7 @@ window.datosTarjetas = [
     id: 1,
     imagen: "../img/restaurante1.png",
     titulo: "Storia D'Amore",
-    enlace: "../html/detalle_res.html",
+    enlace: "../templates/detalle_res.html",
     galeria: [
       "https://sobremesacom.co/wp-content/uploads/2023/06/storia-de-amore.jpg",
       "https://media-cdn.tripadvisor.com/media/photo-s/27/a2/c2/34/storia-d-amore-barranquilla.jpg",
@@ -15,7 +15,7 @@ window.datosTarjetas = [
     id: 2,
     imagen: "../img/restaurante2.png",
     titulo: "Ushin Japanese and Grill",
-    enlace: "../html/detalle_res.html",
+    enlace: "../templates/detalle_res.html",
     galeria:[
       "https://media-cdn.tripadvisor.com/media/photo-s/1a/5b/0e/8c/encuentro-de-detalles.jpg",
       "https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2018/11/14/1057/Grand-Hyatt-Bogota-P211-Ushin-Japanese-and-Grill-Combinado-40-Piezas.jpg/Grand-Hyatt-Bogota-P211-Ushin-Japanese-and-Grill-Combinado-40-Piezas.16x9.jpg?imwidth=1920",
@@ -27,7 +27,7 @@ window.datosTarjetas = [
     id: 3,
     imagen: "../img/restaurante3.png",
     titulo: "Osaka–Cocina Nikkei",
-    enlace: "../html/detalle_res.html",
+    enlace: "../templates/detalle_res.html",
     galeria: [
       "https://media-cdn.tripadvisor.com/media/photo-s/27/c4/32/51/caption.jpg",
       "https://www.buenosbares.com/wp-content/uploads/2023/01/936c5841-e236-ab62-e24f-36e2d2db7098.jpeg",
@@ -39,7 +39,7 @@ window.datosTarjetas = [
     id: 4,
     imagen: "../img/restaurante4.png",
     titulo: "El Capitalino",
-    enlace: "../html/detalle_res.html",
+    enlace: "../templates/detalle_res.html",
     galeria:[
       "https://media-cdn.tripadvisor.com/media/photo-s/15/5c/a7/f8/grand-hyatt-bogota-capitalino.jpg",
       "https://media-cdn.tripadvisor.com/media/photo-s/18/bc/23/a5/ambiente-del-restaurante.jpg",
@@ -51,7 +51,7 @@ window.datosTarjetas = [
     id: 5,
     imagen: "../img/restaurante5.png",
     titulo: "Pajares Salinas",
-    enlace: "../html/detalle_res.html",
+    enlace: "../templates/detalle_res.html",
     galeria: [
       "https://img.mesa247.pe/archivos/platos/pajares-salinas/arros-con-mariscos.webp",
       "https://pajaressalinas.com/wp-content/uploads/sites/3/2021/11/241A3605.jpeg",
@@ -65,27 +65,27 @@ window.datosEventos = [
   {
     imagen: "../img/evento1.png",
     titulo: "Festival Estéreo Picnic",
-    enlace: "../html/detalle_user.html"
+    enlace: "../templates/detalle_user.html"
   },
   {
     imagen: "../img/evento2.png",
     titulo: "Juanpis Live Show",
-    enlace: "../html/detalle_user.html"
+    enlace: "../templates/detalle_user.html"
   },
   {
     imagen: "../img/evento3.png",
     titulo: "Una Idea Genial",
-    enlace: "../html/detalle_user.html"
+    enlace: "../templates/detalle_user.html"
   },
   {
     imagen: "../img/evento4.png",
     titulo: "Cantemos La telenovela",
-    enlace: "../html/detalle_user.html"
+    enlace: "../templates/detalle_user.html"
   },
   {
     imagen: "../img/evento5.png",
     titulo: "Burning Caravan",
-    enlace: "../html/detalle_user.html"
+    enlace: "../templates/detalle_user.html"
   },
 ];
 
@@ -93,26 +93,26 @@ window.datosEmpredimientos= [
   {
     imagen: "../img/emprendimiento1.png",
     titulo: "Domicilios.com",
-    enlace: "../html/detalle_user.html"
+    enlace: "../templates/detalle_user.html"
   },
   {
     imagen: "../img/emprendimiento2.png",
     titulo: "Gruas App",
-    enlace: "../html/detalle_user.html"
+    enlace: "../templates/detalle_user.html"
   },
   {
     imagen: "../img/emprendimiento3.png",
     titulo: "Rappi",
-    enlace: "../html/detalle_user.html"
+    enlace: "../templates/detalle_user.html"
   },
   {
     imagen: "../img/emprendimiento4.png",
     titulo: "Tostaditas Susanita",
-    enlace: "../html/detalle_user.html"
+    enlace: "../templates/detalle_user.html"
   },
   {
     imagen: "../img/emprendimiento5.png",
     titulo: "Dorilocos",
-    enlace: "../html/detalle_user.html"
+    enlace: "../templates/detalle_user.html"
   },
 ];

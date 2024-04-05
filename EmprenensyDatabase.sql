5199fe97a48045c0a54f37ea8ff9dd211c4f9da4
jdavidfa0 <159138481+jdavidfa0@users.noreply.github.com>
2024-04-05 09:19:03 -0500

    Add files via upload

--- EmprenensyDatabase.sql
+++ EmprenensyDatabase.sql
@@ -10,12 +10,13 @@ descripresta varchar(250),
 paginaresta varchar(250),
 comidaresta varchar (250),
 correoresta varchar(50),
-telresta varchar (20),
-redesresta text
+telresta varchar (20)
+
 );
 describe restaurantes;
 alter table restaurantes add column codadmin int;
 alter table restaurantes add constraint fk_restaurantes_codadmin foreign key (codadmin) references administrador (codadmin);
+alter table restaurantes drop column redesresta;
 
 
 alter table restaurantes drop column horarioresta;
@@ -29,15 +30,14 @@ descripeven varchar(250),
 boletaseven varchar (250),
 paginaeven varchar(250),
 correoeven varchar(50),
-televen varchar (20),
-redeseven text
+televen varchar (20)
 
 );
 describe eventos;
 alter table eventos add column codadmin int;
 alter table eventos add constraint fk_eventos_codadmin foreign key (codadmin) references administrador (codadmin);
 alter table eventos drop column  fechaeven;
-
+alter table eventos drop column redeseven;
 
 create table emprendimientos(
 idempre int primary key auto_increment,
@@ -47,13 +47,12 @@ descripempre varchar(250),
 
 paginaempre varchar(250),
 correoempre varchar(50),
-telempre varchar (20),
-nosotrosresta text
+telempre varchar (20)
 
 );
 alter table emprendimientos drop column ubicacionempre;
 alter table emprendimientos drop column horarioempre;
-
+alter table emprendimientos drop column nosotrosresta;
 alter table emprendimientos add column codadmin int;
 alter table emprendimientos add constraint fk_emprendimientos_codadmin foreign key (codadmin) references administrador (codadmin);
 
@@ -139,10 +138,82 @@ foreign key (idempre) references emprendimientos (idempre)
 alter table fechasempre add column horarioempre varchar (250);
 alter table fechaseven add column horarioeven varchar (250);
 
+create table redes(
+codredes int primary key auto_increment,
+tipored varchar (30),
+urlred text,
+idresta int,
+ideven int,
+idempre int,
+foreign key(idresta)references restaurantes(idresta),
+foreign key (ideven)references eventos (ideven),
+foreign key(idempre)references emprendimientos(idempre)
+);
+
 
 drop table if exists fechasresta;
 
 use emprenesy;
+
+create table publi_resta(
+codpubli int primary key auto_increment,
+descrip_publi varchar(250),
+codgaleria int,
+fechapubli date,
+idresta int,
+codadmin int,
+foreign key (idresta) references restaurantes (idresta),
+foreign key (codadmin)references administrador (codadmin),
+foreign key (codgaleria)references galeriaresta(codgaleria)
+);
+
+create table publi_even(
+codpubli int primary key auto_increment,
+descrip_publi varchar(250),
+codgaleria int,
+fechapubli date,
+ideven int,
+codadmin int,
+foreign key (ideven) references eventos (ideven),
+foreign key (codadmin)references administrador (codadmin),
+foreign key (codgaleria)references galeriaeven(codgaleria)
+);
+
+create table publi_empre(
+codpubli int primary key auto_increment,
+descrip_publi varchar(250),
+codgaleria int,
+fechapubli date,
+idempre int,
+codadmin int,
+foreign key (idempre) references emprendimientos (idempre),
+foreign key (codadmin)references administrador (codadmin),
+foreign key (codgaleria)references galeriaempre(codgaleria)
+);
+
+create table favoritos(
+codfav int primary key auto_increment,
+codusuario int,
+codpubli_resta int,
+codpubli_even int,
+codpubli_empre int,
+idresta int,
+ideven int,
+idempre int,
+foreign key (codusuario) references usuario (codusuario),
+foreign key (codpubli_resta) references publi_resta(codpubli),
+foreign key (codpubli_even) references publi_even(codpubli),
+foreign key (codpubli_empre) references publi_empre(codpubli),
+foreign key (idresta) references restaurantes (idresta),
+foreign key (ideven) references eventos(ideven),
+foreign key (idempre) references emprendimientos (idempre)
+);
+
+
+
+
+ drop table if exists publi_resta;
+
 
 create table usuario(
 codusuario int primary key auto_increment,

b480a0c98d27ecdfee3860436aca6749d384bdaf
pulidoibarra15@gmail.com <pulidoibarra15@gmail.com>
2024-04-29 15:00:42 -0500

    proyecto estatico

--- proyectoStatic/js/generales_admin.js
+++ proyectoStatic/js/generales_admin.js
@@ -1,25 +1,6 @@
 function user_sesion() {
-    fetch('/admin/perfil_imagen')
-        .then(response => {
-            if (!response.ok) {
-                //indica si la solicitud no fue existosa
-                throw new Error('Network response was not ok');
-            }
-            return response.blob();
-        })
-        .then(imageBlob => {
-            const imagenURL = URL.createObjectURL(imageBlob);
-            crearNav(imagenURL)
-        })
-        .catch(error => {
-            console.error('Error al cargar la imagen del perfil:', error);
-        });
-}
-
-function crearNav(imagenURL) {
-
     const menuItems = [
-        { text: `<img src="${imagenURL}" alt="perfil">`, href: '#', class: 'link1', hasSubMenu: true }
+        { text: '<img src="/static/img/perfil_user.png" alt="perfil">', href: '#', class: 'link1', hasSubMenu: true }
     ];
 
     // Elementos del submenu
@@ -36,36 +17,21 @@ function crearNav(imagenURL) {
     const nav_list = document.createElement('ul');
     nav_list.className = 'nav_list';
 
-    crearBuscadorNav(nav_list)
-    crearMenuItem(nav_list, menuItems, subMenuItems);
-
-    nav_user.appendChild(nav_list);
-
-    const header_barra = document.createElement('div');
-    header_barra.id = "header_barra";
-    header_barra.appendChild(nav_user);
-
-    const header = document.querySelector('#cabeza');
-    header.appendChild(header_barra);
-}
-
-function crearBuscadorNav(nav_list) {
-
     // Crear elemento li para el buscador
     const buscador_li = document.createElement('li');
     buscador_li.innerHTML = `
-        <div class="header_buscador">
-            <div class="buscador_input">
-                <div id="ctn-bars-search">
-                    <input type="text" id="inputSearch" placeholder="¿Qué deseas buscar?">
-                </div>
-                <div id="ctn-icon-search">
-                    <i class="fas fa-search" id="icon-search"></i>
-                </div>
+    <div class="header_buscador">
+        <div class="buscador_input">
+            <div id="ctn-bars-search">
+                <input type="text" id="inputSearch" placeholder="¿Qué deseas buscar?">
             </div>
-            <ul id="box-search"></ul>
-            <div id="cover-ctn-search"></div>
-        </div>`;
+            <div id="ctn-icon-search">
+                <i class="fas fa-search" id="icon-search"></i>
+            </div>
+        </div>
+        <ul id="box-search"></ul>
+        <div id="cover-ctn-search"></div>
+    </div>`;
     nav_list.appendChild(buscador_li);
 
     const icon_a = document.createElement('a');
@@ -74,12 +40,9 @@ function crearBuscadorNav(nav_list) {
 
     const icon = document.createElement('i');
     icon.classList.add('bi', 'bi-bell-fill');
-
     icon_a.appendChild(icon);
     nav_list.appendChild(icon_a);
-}
 
-function crearMenuItem(nav_list, menuItems, subMenuItems) {
     // Crear elementos li y a para el menú principal
     menuItems.forEach(item => {
         const liElement = document.createElement('li');
@@ -87,8 +50,9 @@ function crearMenuItem(nav_list, menuItems, subMenuItems) {
             const aElement = document.createElement('a');
             aElement.className = item.class;
             aElement.href = item.href;
-            aElement.innerHTML = item.text;
+            aElement.innerHTML = item.text; // Si quieres usar HTML aquí, esto funciona
             liElement.appendChild(aElement);
+
             const subMenu = document.createElement('ul');
             subMenu.className = 'perfil_list';
             subMenuItems.forEach(subItem => {
@@ -100,7 +64,6 @@ function crearMenuItem(nav_list, menuItems, subMenuItems) {
                 subMenu.appendChild(subLiElement);
             });
             liElement.appendChild(subMenu);
-
         } else {
             const aElement = document.createElement('a');
             aElement.className = item.class;
@@ -110,6 +73,15 @@ function crearMenuItem(nav_list, menuItems, subMenuItems) {
         }
         nav_list.appendChild(liElement);
     });
+
+    nav_user.appendChild(nav_list);
+
+    const header_barra = document.createElement('div');
+    header_barra.id = "header_barra";
+    header_barra.appendChild(nav_user);
+
+    const header = document.querySelector('#cabeza');
+    header.appendChild(header_barra);
 }
 
 function menu_lateral() {

620991057fc2c947f4576b71868014a7335014c8
pulidoibarra15@gmail.com <pulidoibarra15@gmail.com>
2024-05-06 16:24:15 -0500

    pop up eventos formulario

--- static/js/alertas.js
+++ static/js/alertas.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const formulario = document.getElementById('formularioLocation');
-    const redireccionar = document.getElementById('form_url').getAttribute('data-url');
+    const btnRegresar = document.getElementById('btn_regresar');
+    const redireccionar = document.getElementById('index').getAttribute('data-url');
+    const redireccionarForm = document.getElementById('form_evento').getAttribute('data-url');
 
     formulario.addEventListener('submit', (e) => {
-        e.preventDefault();
-
+        //e.preventDefault();
         Swal.fire({
             position: "center",
             icon: "success",
             title: "Registro de publicación exitosa",
             showConfirmButton: false,
-            timer: 3500,
+            timer: 4000,
             customClass: {
                 popup: 'border-blue', // Clase CSS para el borde del SweetAlert
                 icon: 'success-icon',
@@ -20,4 +21,53 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.href = redireccionar;
         });
     });
+
+    btnRegresar.addEventListener('click', () => {
+        Swal.fire({
+            title: "¿Estás seguro de que deseas regresar?",
+            text: "Si regresas, podrías tener que reingresar algunos datos.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Sí, deseo regresar!",
+            cancelButtonText: "Cancelar",
+            customClass: {
+                popup: 'border-blue',
+                icon: 'success-icon',
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Si el usuario confirma, redirigir a la página especificada
+                Swal.fire({
+                    title: "Volviste al formulario anterior",
+                    text: "Verifica los datos antes de continuar",
+                    icon: "info",
+                    timer: 4000,
+                    customClass: {
+                        popup: 'border-blue',
+                        icon: 'success-icon',
+                    }
+                }).then(() => {
+
+                    window.location.href = redireccionarForm;
+                });
+
+            } else {
+                // Si el usuario cancela, simplemente cerrar la alerta y permanecer en la página
+                Swal.fire({
+                    title: "Cancelado",
+                    text: "Continúa con el formulario actual.",
+                    icon: "info",
+                    timer: 2000,
+                    customClass: {
+                        popup: 'border-blue',
+                        icon: 'success-icon',
+                    }
+                });
+            }
+        });
+    });
+
+
 });

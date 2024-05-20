9057129706e5be927cdd5ae69fe83d697ce2ba2e
paula pulido <juliaan.roy@gmail.com>
2024-05-20 09:28:53 -0500

    registros

--- baseDatos/registros.sql
+++ baseDatos/registros.sql
@@ -35,8 +35,88 @@ VALUES (1, 'galeriaRes/frenesi1.jpg', 'Imágen de restaurante'),
 INSERT INTO ubicacionresta (idresta, ubicacion) 
 VALUES (1, 'Autopista Norte No. 114 - 44, Bogotá');
 
-SELECT * FROM redes_sociales
-WHERE entidad_id = 6 AND entidad_tipo = 'restaurante';
+
+#**********************************************************************************************************************
+INSERT INTO restaurantes (nombreresta, logo, tiporesta, descripresta, paginaresta, menu, horario, horarioApertura,
+ horarioCierre, correoresta, telresta, fecha_publicacion, codadmin) 
+VALUES ('OKA Grill House', 'galeriaRes/oka grill logo.png', 'Gourmet', 'OKA Grill House es una parrilla al carbón atrevida y contemporánea que rinde homenaje a la tradición culinaria de Colombia. Refleja la versatilidad y el placer de ingredientes locales en preparaciones internacionales. El ambiente es moderno, con una cocina abierta, amplia terraza al aire libre y decoración única inspirada en la naturaleza colombiana y la Leyenda del Dorado',
+ 'https://www.facebook.com/OkaGrillHouse/', 'https://oka-grill-house.cluvi.co/oka-grill-house/maincategories', 'Abierto todos los días', '06:00:00', '22:00:00',
+ null, ' 601 4434438', '2024-05-15', 2);
+ 
+INSERT INTO redes_sociales (entidad_id, entidad_tipo, red, url) 
+VALUES (1, 'restaurante', 'Instagram', 'https://www.instagram.com/okagrillhouse/?hl=es'),
+       (1, 'restaurante', 'TikTok', 'https://www.tiktok.com/discover/oka-grill-house-buffet-bogota');
+
+INSERT INTO galeriaresta (idresta, imagenresta, descripcion) 
+VALUES (1, 'galeriaRes/oka1.jpg', 'Imágen de restaurante'),
+       (1, 'galeriaRes/oka2.jpg', 'Imágen de restaurante'),
+       (1, 'galeriaRes/oka3.jpg', 'Imágen de restaurante'),
+       (1, 'galeriaRes/oka4.jpg', 'Imágen de restaurante');
+INSERT INTO ubicacionresta (idresta, ubicacion) 
+VALUES (1, 'Carrera 37 24 29 Hilton Bogotá');
+
+#**********************************************************************************************************************
+INSERT INTO restaurantes (nombreresta, logo, tiporesta, descripresta, paginaresta, menu, horario, horarioApertura,
+ horarioCierre, correoresta, telresta, fecha_publicacion, codadmin) 
+VALUES ('Home Burgers', 'galeriaRes/burgerLogo.png', 'Gourmet', 'Home Burgers trae el concepto de fast casual a Colombia, con hamburguesas clásicas, hechas con ingredientes de la mejor calidad, deliciosas y frescas',
+ 'https://homeburgers.com/', 'https://homeburgers.com/', 'Abierto todos los días', '11:30:00', '22:00:00',
+ null, '1 5554390', '2024-05-15', 2);
+ 
+INSERT INTO redes_sociales (entidad_id, entidad_tipo, red, url) 
+VALUES (1, 'restaurante', 'Instagram', 'https://www.instagram.com/homeburgers_/'),
+       (1, 'restaurante', 'TikTok', 'https://www.tiktok.com/@homeburgers_');
+
+INSERT INTO galeriaresta (idresta, imagenresta, descripcion) 
+VALUES (1, 'galeriaRes/burger1.jpeg', 'Imágen de restaurante'),
+       (1, 'galeriaRes/burger2.jpg', 'Imágen de restaurante'),
+       (1, 'galeriaRes/burger3.jpg', 'Imágen de restaurante'),
+       (1, 'galeriaRes/burger4.jpg', 'Imágen de restaurante');
+       
+INSERT INTO ubicacionresta (idresta, ubicacion) 
+VALUES (1, 'Carrera 9 81a-19, Chapinero Bogotá'),
+(2, 'AV. Boyacá # 145-62 Bogotá'),
+(3, 'Cl 140 # 10A-76 Bogotá'),
+(4,'Cl 185 # 45-03 Bogotá');
+
+#**********************************************************************************************************************
+INSERT INTO restaurantes (nombreresta, logo, tiporesta, descripresta, paginaresta, menu, horario, horarioApertura,
+ horarioCierre, correoresta, telresta, fecha_publicacion, codadmin) 
+VALUES ('Imagine Restaurante', 'galeriaRes/imagineLogo.png', 'Restaurante temático', 'Ubicado en el sector de Corferias, al lado del Hotel Black Tower Premium Bogotá, Imagine es un restaurante temático inspirado en los Beatles y lo mejor de la época. Ofrecen una carta muy variada de comida internacional y una selección especial de los mejores platos colombianos que hacen honor a nuestra tierra y sus sabores',
+ 'https://www.blacktowerhotel.com/es/imagine-restaurant.html', 'https://qr.precompro.com/?source=imagine.precompro.com', 'Abierto todos los días', '10:00:00', '22:00:00',
+ null, '318 8365751', '2024-05-15', 2);
+ 
+INSERT INTO redes_sociales (entidad_id, entidad_tipo, red, url) 
+VALUES (1, 'restaurante', 'Instagram', 'https://www.instagram.com/imaginerestaurante/'),
+       (1, 'restaurante', 'TikTok', 'https://www.tiktok.com/discover/imagine-restaurante-bogot%C3%A1');
+
+INSERT INTO galeriaresta (idresta, imagenresta, descripcion) 
+VALUES (1, 'galeriaRes/imagine1.jpg', 'Imágen de restaurante'),
+       (1, 'galeriaRes/imagine2.jpg', 'Imágen de restaurante'),
+       (1, 'galeriaRes/imagine3.jpg', 'Imágen de restaurante'),
+       (1, 'galeriaRes/imagine4.jpg', 'Imágen de restaurante');
+       
+INSERT INTO ubicacionresta (idresta, ubicacion) 
+VALUES (1, 'Avenida Calle 24 # 43 a 21, Hotel Black Tower Premium Bogotá');
+
+#**********************************************************************************************************************
+INSERT INTO restaurantes (nombreresta, logo, tiporesta, descripresta, paginaresta, menu, horario, horarioApertura,
+ horarioCierre, correoresta, telresta, fecha_publicacion, codadmin) 
+VALUES ('Mirador La Paloma', 'galeriaRes/palomaLogo.png', ' Este restaurante ofrece una vista espectacular de Bogotá y es conocido por su ambiente romántico. Además, cuenta con música en vivo para celebraciones especiales. Los platos son deliciosos y los cócteles están a la altura. Sin duda, un lugar para disfrutar de una experiencia gastronómica única',
+ 'https://miradorlapaloma.com/', 'https://www.qrcarta.com/restaurant/bogota/mirador-la-paloma/2987/', 'Abierto todos los días', '12:00:00', '03:00:00',
+ ' www.miradorlapaloma@hotmail.com', '3144444955', '2024-05-15', 2);
+ 
+INSERT INTO redes_sociales (entidad_id, entidad_tipo, red, url) 
+VALUES (1, 'restaurante', 'Instagram', 'https://www.instagram.com/miradorlapaloma/?hl=es'),
+       (1, 'restaurante', 'TikTok', 'https://www.tiktok.com/discover/restaurante-mirador-la-paloma?lang=es');
+
+INSERT INTO galeriaresta (idresta, imagenresta, descripcion) 
+VALUES (1, 'galeriaRes/paloma1.jpg', 'Imágen de restaurante'),
+       (1, 'galeriaRes/paloma2.jpg', 'Imágen de restaurante'),
+       (1, 'galeriaRes/paloma3.jpg', 'Imágen de restaurante'),
+       (1, 'galeriaRes/paloma4.jpg', 'Imágen de restaurante');
+       
+INSERT INTO ubicacionresta (idresta, ubicacion) 
+VALUES (1, 'Kilómetro 1 vía a La Calera, Bogotá');
 
 #**********************************************************************************************************************
 INSERT INTO emprendimientos (nombreempre, logo, tipoempre, descripempre, horarioempre, horarioApertura, 
@@ -77,3 +157,23 @@ VALUES (1, 'galeriaEmprende/artesaniasColombia2.jpg', 'Imagen del emprendimiento
 
 INSERT INTO ubicacionempre (idempre, ubicacion) 
 VALUES (1, 'Calle 74 No. 11-91, Bogotá'),(1, 'Carrera 11 No. 84 - 12, Bogotá');
+
+#**********************************************************************************************************************
+INSERT INTO emprendimientos (nombreempre, logo, tipoempre, descripempre, horarioempre, horarioApertura, 
+horarioCierre, paginaempre, producempre, correoempre, telempre, fecha_publicacion, codadmin) 
+VALUES ('Wata', 'galeriaEmprende/artesaniasColombia1.png', 'Ropa', ' Wata es una empresa de Bogotá que se ha dedicado durante los últimos dos años a fabricar colecciones de ropa que no solo te hagan ver y sentir bien, sino que también ayudan al planeta. Su enfoque está en la moda sostenible y la producción ética, sin causar efectos secundarios o impactar negativamente la naturaleza', 
+'Lunes-Sábado', '09:00:00', '14:00:00', 'https://m.facebook.com/wata.col',
+ 'https://artesaniasdecolombia.com.co/PortalAC/Catalogo/CatalogoIndex.jsf', 'soytransparente@artesaniasdecolombia.com.co', '312 5806209', '2024-05-15', 2);
+
+INSERT INTO redes_sociales (entidad_id, entidad_tipo, red, url) 
+VALUES (1, 'emprendimiento', 'Instagram', 'https://www.instagram.com/bywata/?hl=es-la'),
+       (1, 'emprendimiento', 'TikTok', null);
+       
+INSERT INTO galeriaempre (idempre, imagenempre, descripcion) 
+VALUES (1, 'galeriaEmprende/artesaniasColombia2.jpg', 'Imagen del emprendimiento'),
+       (1, 'galeriaEmprende/artesaniasColombia3.jpg', 'Imagen del emprendimiento'),
+       (1, 'galeriaEmprende/artesaniasColombia4.jpg', 'Imagen del emprendimiento'),
+       (1, 'galeriaEmprende/artesaniasColombia5.jpg', 'Imagen del emprendimiento');
+
+INSERT INTO ubicacionempre (idempre, ubicacion) 
+VALUES (1, 'Cl. 70 # 27b - 07, Barrios Unidos, Bogotá');

b7def8cd6a0e419071d1e7619c9f898e8af94302
pulidoibarra15@gmail.com <pulidoibarra15@gmail.com>
2024-05-23 09:35:48 -0500

    pop up

--- static/js/sesion.js
+++ static/js/sesion.js
@@ -1,33 +1,51 @@
-document.getElementById("loginForm").addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita el envío del formulario por defecto
-
-    // Captura los datos del formulario
-    var correo = document.getElementById("usuario").value;
-    var contrasena = document.getElementById("contrasena").value;
-    var rol = document.querySelector('input[name="rol"]:checked').value;
-
-    // Guarda los datos en el almacenamiento local
-    localStorage.setItem("correo", correo);
-    localStorage.setItem("contrasena", contrasena);
-    localStorage.setItem("rol", rol);
-
-    // Muestra un mensaje de éxito
-    Swal.fire({
-        position: "center",
-        icon: "success",
-        title: "Has iniciado sesión correctamente",
-        showConfirmButton: false,
-        timer: 2000,
+function verificacionContrasenas(event) {
+    event.preventDefault(); // Evita el envío del formulario inmediatamente
+  
+    const contrasena = document.getElementById('contrasena').value;
+    const email = document.getElementById('usuario').value;
+    const tipoRol = document.getElementById('rol').value;
+  
+    if (contrasena === '' || email === '' || tipoRol === '') {
+      Swal.fire({
+        title: "Todos los campos son obligatorios",
+        showClass: {
+          popup: `
+                    animate__animated
+                    animate__fadeInUp
+                    animate__faster
+                `
+        },
+        hideClass: {
+          popup: `
+                    animate__animated
+                    animate__fadeOutDown
+                    animate__faster
+                `
+        },
         customClass: {
-            popup: 'border-blue',// Clase CSS para el borde del SweetAlert
-            icon: 'success-icon',
+          popup: 'border-blue',
+          icon: 'success-icon',
         }
-    }).then(function () {
-        localStorage.setItem('usuarioRegistrado', 'true');
-        if (rol === 'usuario') {
-            window.location.href = '{{ url_for("usuarios.perfil_usuario") }}';
-        } else if (rol === 'Administrador') {
-            window.location.href = '{{ url_for("admin.perfil_admin") }}';
-        }
+      });
+  
+      return false; // Detiene el envío del formulario
+    }
+  
+    // Mostrar mensaje de éxito
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Bienvenido a Emprenesy",
+      showConfirmButton: false,
+      timer: 1500,
+      customClass: {
+        popup: 'border-blue', 
+        icon: 'success-icon',
+      }
+    }).then(() => {
+      // Enviar el formulario después de mostrar el mensaje de éxito
+      document.getElementById('loginForm').submit();
     });
-});
+  
+    return true;
+  }

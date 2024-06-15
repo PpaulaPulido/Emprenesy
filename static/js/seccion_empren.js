7b3e2dbd1f7f11a8b6d124398f042105bba7c55a
pulidoibarra15@gmail.com <pulidoibarra15@gmail.com>
2024-06-15 18:12:57 -0500

    querys en secciones

--- static/js/seccion_empren.js
+++ static/js/seccion_empren.js
@@ -1,15 +1,3 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: false,
-    navigation: {
-        prevEl: ".swiper-button-prev",
-        nextEl: ".swiper-button-next",
-    },
-});
-
 document.addEventListener('DOMContentLoaded', function () {
 
     const swiper = document.getElementById('swiper');
@@ -19,4 +7,25 @@ document.addEventListener('DOMContentLoaded', function () {
     tarjetas_swiper(datosEmpredimientos, swiper,detalleEmprendeUrl);
     tarjetas_swiper(empredimientosArtesania, swiper2,detalleEmprendeUrl);
     manejarFavoritos('favoritosEm');
+
+    let cardSlider = 3;
+    if (window.innerWidth < 920) {
+        cardSlider = 1;
+    } else if (window.innerWidth < 1024) {
+        cardSlider = 2;
+    }
+
+    const swiper3 = new Swiper(".mySwiper", {
+        slidesPerView: cardSlider,
+        spaceBetween: 20,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: false,
+        navigation: {
+            prevEl: ".swiper-button-prev",
+            nextEl: ".swiper-button-next",
+        }
+    });
 });
+
+
